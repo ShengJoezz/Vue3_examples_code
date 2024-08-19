357f33451653a1ca4308d0288f78bf2be6efb856
joe <847304926@qq.com>
2024-08-19 23:15:03 +0800

    add new code for examples code

---
+++ attribute_Binding.vue
@@ -0,0 +1,22 @@
+<template>
+    <p :title="msg">Hover your mouse over me for a few seconds to see my dynamically bound title!</p>
+    <p :class="{red : isRed}" @click='toggleRed'>This should be red... but click me to toggle it.</p>
+    <p :style="{color : isGreen?'green':'blue'}" @click="toggleGreenBlue">This should be green, and should toggle between green and blue on click.</p>
+  </template>
+  <script setup>
+    import {ref} from 'vue'
+    const msg=ref('Hello!')
+    const isRed=ref(true)
+    const isGreen=ref(true)
+    function toggleRed(){
+      isRed.value = !isRed.value
+    }
+    function toggleGreenBlue(){
+      isGreen.value = !isGreen.value
+    }
+  </script>
+  <style>
+    .red{
+      color:red;
+    }
+  </style>
---
+++ condition_and_Loop.vue
@@ -0,0 +1,38 @@
+<template>
+    <button @click="toggleList">Toggle List</button>
+    <button @click="pushNumber">Push Number</button>
+    <button @click="popNumber">Pop Number</button>
+    <button @click="reverseList">Reverse List</button>
+    <ul v-if="isShow && !isEmpty">
+      <li v-for="n in List" :key="n">{{n}}</li>
+    </ul>
+    <p v-else-if="isEmpty">The List is empty!</p>
+    <p v-else>You Hide The List!</p>
+  </template>
+  <script setup>
+    import {ref,computed} from 'vue'
+    const num=ref(1)
+    const List=ref([num.value,++num.value,++num.value])
+    const isShow = ref(true)
+    const isEmpty = ref(false)
+    //const isEmpty=computed(()=>List.value.length === 0)
+    function toggleList(){
+      isShow.value = !isShow.value
+    }
+    function pushNumber(){
+      List.value.push(++num.value)
+    }
+    function popNumber(){
+      if(num.value >0){
+      List.value.pop()
+      num.value--}
+      else{
+        num.value=0
+        isEmpty.value=true
+      }
+    }
+    //更好的写法应该是:const isEmpty=computed(()=>List.value.length === 0)
+    function reverseList(){
+      List.value.reverse()
+    }
+  </script>
